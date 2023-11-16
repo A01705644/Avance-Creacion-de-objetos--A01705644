f188a901722b24b16f89fd5f5b24d913be04fa5f
A01705644 <150179445+A01705644@users.noreply.github.com>
2023-11-15 23:42:47 -0600

    Update and rename orden tacos .h to tacos .h
    Se cambió el nombre para uso correcto y se cambiaron las descripciones de algunos atributos así como se borraron algunos antiguos

--- tacos .h
+++ tacos .h
@@ -5,38 +5,36 @@
 //  Created by Sebas Muñoz on 08/11/23.
 //
 
-#ifndef orden_tacos_h
-#define orden_tacos_h
+#ifndef tacos_h
+#define tacos_h
 #include <string>
 
 
-//Atributos de la clase donde están el tipo de taco o carne que se le va a poner(tipo_tacos), número de tacos que pidieron en la orden (orden)si el cliente dijo que quería o no salsa donde True significa que sí y False que no (salsa), si el cliente dijo que quería o no cilantro donde True significa que sí y False que no (cilantro),si el cliente dijo que quería o no cebolla donde True significa que sí y False que no (cebolla), el precio de las tacos (precio_taco) la cuenta que es lo que costó la orden  completa (total)
+//Atributos de la clase donde están el tipo de taco o carne que se le va a poner(tipo_tacos), número de tacos que pidieron en la orden (orden)si el cliente dijo que quería o no salsa donde True significa que sí y False que no (salsa), si el cliente dijo que quería o no cilantro y cebolla donde True significa que sí y False que no (cilantro_cebolla), el precio de las tacos (precio_taco) y la cuenta que es lo que costó la orden  completa (total)
 class Tacos{
 private:
     std::string tipo_tacos;
     int orden;
     bool salsa;
-    bool cilantro;
-    bool cebolla;
+    bool cilantro_cebolla;
     int precio_taco=10;
     float total;
     
 public:
     // El constructor de la clase con sus parámetros y los métodos que tiene esta clase
     Tacos();
-    Tacos (int cuantos,std::string tac, bool sal, bool cil, bool ceb){
+    Tacos (int cuantos,std::string tac, bool sal, bool cilceb){
         tipo_tacos= tac;
         orden=cuantos;
         salsa=sal;
-        cilantro=cil;
-        cebolla=ceb;
+        cilantro_cebolla=cilceb;
+
     };
     //Métodos get de los atributos de la clase
     std::string get_tipo_tacos();
     int get_orden();
     bool get_salsa();
-    bool get_cilantro();
-    bool get_cebolla();
+    bool get_cilantro_cebolla();
     
     //Método para calcular el total de la cuenta por la compra de las tortas
     float get_la_cuenta();
@@ -45,8 +43,7 @@ public:
     void set_tipo_tacos(std::string);
     void set_orden(int);
     void set_salsa(bool);
-    void set_cebolla(bool);
-    void set_cilantro(bool);
+    void set_cilantro_cebolla(bool);
     
 
 };
@@ -75,21 +72,14 @@ void Tacos:: set_salsa(bool sal){
     salsa=sal;
 }
 
-bool Tacos:: get_cilantro(){
-    return cilantro;
+bool Tacos:: get_cilantro_cebolla(){
+    return cilantro_cebolla;
 }
 
-void Tacos:: set_cilantro(bool cil){
-    cilantro=cil;
+void Tacos:: set_cilantro_cebolla(bool cilceb){
+    cilantro_cebolla=cilceb;
 }
 
-bool Tacos:: get_cebolla(){
-    return cebolla;
-}
-
-void Tacos:: set_cebolla(bool ceb){
-    cebolla=ceb;
-}
 
 //El total de la cuenta se calcula antes de mandar a llamar el valor de total con el mismo getter y es la multplicación de el precio fijo de los tacos por el número de tacos (orden) que se pidieron con la orden
 
@@ -99,6 +89,9 @@ float Tacos::get_la_cuenta(){
     return total;
 }
 
-#endif //* orden_tacos_h */
+#endif //* tacos_h */
+
+
+
 
 
